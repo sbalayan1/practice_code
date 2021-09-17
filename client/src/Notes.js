67cdbf7aac99338c34d602a96c2c3cb19a662cb4
sbalayan1 <balayans2014@yahoo.com>
2021-09-17 14:52:48 -0700

    redux notes

--- client/src/Notes.js
+++ client/src/Notes.js
@@ -64,3 +64,19 @@ Time = O(1) + O(n) + O(n^2) = O(n^2) -> because O(n^2) dominates the lower order
 What is a permutation? 
 - When order DOESN'T matter, it is called a combination 
 - When order DOES matter, it is called a permutation
+
+
+9/18/21 REDUX Notes
+What is a reducer?
+- A reducer combines the current state and an action, reducing the combo into one value or into one updated state. 
+
+ex. action -> reducer -> updated state 
+
+Characteristics of a reducer 
+- Pure functions are only determined by their input values
+- Pure Functions have no side effects. By this we mean pure functions do not have any effect outside of the function. They only return a value.
+
+Redux Flow 
+    1. We hold our application's state in one plain old JavaScript object, and we update that state by passing both an action and the old state to our reducer. Our reducer returns to us our new state.
+    2. So to change our state we (1) create an action (an action is just a plain object with a type key); and (2) and pass the action as an argument when we call the reducer (which is just a function with a switch/case statement). This produces a new state.
+    3. Our reducer is a pure function which means that given the same arguments of state and action, it will always produce the same new state. Also it means that our reducer never updates the previous state, but rather creates a new state object.
