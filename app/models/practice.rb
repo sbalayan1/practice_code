dc11231ddb974b6a7cc5baad360690d8a5326a5d
sbalayan1 <balayans2014@yahoo.com>
2021-09-17 14:13:58 -0700

    code wars problem on anagrams

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -171,3 +171,48 @@ Best Solution:
 def panagram?(string)
   ('a'..'z').all? { |x| string.downcase.include? (x) } 
 end
+
+5. What is an anagram? Well, two words are anagrams of each other if they both contain the same letters. For example:
+
+'abba' & 'baab' == true
+
+'abba' & 'bbaa' == true
+
+'abba' & 'abbba' == false
+
+'abba' & 'abca' == false
+Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example:
+
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+
+anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+Note for Go
+For Go: Empty string slice is expected when there are no anagrams found.
+
+
+Solution: 
+def anagrams(word, words)
+  
+  target_size = word.size 
+  target_sorted = word.each_char.sort
+
+  def anagram?(target_size, target_sorted, w)
+    return false unless target_size === w.size 
+    w.each_char.sort === target_sorted 
+  end 
+
+  words.select{|w| anagram?(target_size, target_sorted, w)}
+
+end
+
+Best Solution: 
+  def anagrams(word, words)
+    words.select { |w| w.chars.sort == word.chars.sort }
+  end 
+
+Clever Solution: 
+  def anagrams(word, words)
+    words.select{|w| w.sum == word.sum}
+  end
