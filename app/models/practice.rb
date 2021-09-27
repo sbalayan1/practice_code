5f66da829227fe43e637d87dc3c6cc2ec4ff5133
sbalayan1 <balayans2014@yahoo.com>
2021-09-27 07:21:45 -0700

    building blocks codewars kata

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -255,3 +255,42 @@ Best Solution:
 function disemvowel(str) {
   return str.replace(/[aeiou]/gi, '');
 }
+
+
+7. Your task is to construct a building which will be a pile of n cubes. The cube at the bottom will have a volume of n^3, the cube above will have volume of (n-1)^3 and so on until the top which will have a volume of 1^3.
+
+You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
+
+The parameter of the function findNb (find_nb, find-nb, findNb, ...) will be an integer m and you have to return the integer n such as n^3 + (n-1)^3 + ... + 1^3 = m if such a n exists or -1 if there is no such n.
+
+Examples:
+findNb(1071225) --> 45
+
+findNb(91716553919377) --> -1
+
+Solution: 
+function findNb(m) {
+
+# //   create a for loop that increases n infinitely 
+  for (let n = 0;;n++) {
+
+# // Subtract the current value of the building (n+1^3) from the max if m>0. Note that n+1 is done to increment n. 
+    if (m > 0) {
+      m = m - Math.pow(n+1, 3)
+      
+# // Otherwise return -1 or n depending on the value of m.
+    } else if (m === 0 ) {
+      return n
+      
+    } else {      
+      return -1
+    }
+  }
+}
+
+Best Solution/Clever: 
+function findNb(m) {
+  var n = 0
+  while (m > 0) m -= ++n**3
+  return m ? -1 : n
+}
