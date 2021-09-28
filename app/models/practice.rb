4ffc4d1db220263d84a3226557b6f1e19f7e640d
sbalayan1 <balayans2014@yahoo.com>
2021-09-27 18:20:35 -0700

    codewars kata. find max sequence

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -317,3 +317,36 @@ Solution:
 
 Best Solution: 
   const number = (busStops) => busStops.reduce((rem, [on, off]) => rem + on - off, 0);
+
+
+  The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+
+  maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+  // should be 6: [4, -1, 2, 1]
+  Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
+  
+  Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+
+Solution: 
+
+let maxSequence = (arr) => {
+  let allCombinations = []
+  
+  if (arr.length === 0 || arr.find(num => num>0) === undefined) {
+    return 0
+    
+  } else if (arr.find(num => num<0) === undefined) {
+    return arr.reduce((a,b) => a+b, 0)
+    
+    
+  } else {
+    for (let i = 0; i<arr.length; i++) {
+      for (let j = i+1; j<arr.length; j++) {
+        let comboArr = arr.slice(i,j)      
+        allCombinations.push(comboArr.reduce((a,b) => a+b,0))
+      }
+    }
+  }
+    
+  return Math.max(...allCombinations)  
+}
