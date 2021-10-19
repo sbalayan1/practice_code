b01e6cfe3af2d2227485f86b00ea71f1a16618d8
sbalayan1 <balayans2014@yahoo.com>
2021-10-19 16:16:33 -0700

    complete codewars factorial problem

--- client/src/Notes.js
+++ client/src/Notes.js
@@ -1006,3 +1006,47 @@ Solution:
 
     return filter0
   }
+
+10. Description:
+// Lyrics...
+// Pyramids are amazing! Both in architectural and mathematical sense. If you have a computer, you can mess with pyramids even if you are not in Egypt at the time. For example, let's consider the following problem. Imagine that you have a pyramid built of numbers, like this one here:
+
+//    /3/
+//   \7\ 4 
+//  2 \4\ 6 
+// 8 5 \9\ 3
+// Here comes the task...
+// Let's say that the 'slide down' is the maximum sum of consecutive numbers from the top to the bottom of the pyramid. As you can see, the longest 'slide down' is 3 + 7 + 4 + 9 = 23
+
+// Your task is to write a function longestSlideDown (in ruby/crystal/julia: longest_slide_down) that takes a pyramid representation as argument and returns its' largest 'slide down'. For example,
+
+// longestSlideDown([[3], [7, 4], [2, 4, 6], [8, 5, 9, 3]]) => 23
+// By the way...
+// My tests include some extraordinarily high pyramids so as you can guess, brute-force method is a bad idea unless you have a few centuries to waste. You must come up with something more clever than that.
+
+// (c) This task is a lyrical version of the Problem 18 and/or Problem 67 on ProjectEuler.
+
+Solution: 
+
+let longestSlideDown = (pyramid) => {
+  return pyramid.reduceRight((last, current) => {
+    return current.map((v, i) => v + Math.max(last[i], last[i+1]))
+  })[0]
+}
+
+// 11. Your task is to write function factorial.
+
+Solution (iteratively): 
+
+let factorial = (n) => {
+  let product = 1
+  if (n <= 1) {
+    return product
+  } else {
+    for (let i = n; i>=1;  i--) {
+      product = product * i
+    }
+  }
+
+  return product
+}
