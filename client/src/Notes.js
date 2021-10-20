b832065a0c361dc788cce609d4af778d3334b2ad
sbalayan1 <balayans2014@yahoo.com>
2021-10-19 17:29:55 -0700

    complete codewars fibonacci numbers recursively and iteratively

--- client/src/Notes.js
+++ client/src/Notes.js
@@ -1038,15 +1038,71 @@ let longestSlideDown = (pyramid) => {
 
 Solution (iteratively): 
 
-let factorial = (n) => {
-  let product = 1
-  if (n <= 1) {
-    return product
-  } else {
-    for (let i = n; i>=1;  i--) {
-      product = product * i
+  let factorial = (n) => {
+    let product = 1
+    if (n <= 1) {
+      return product
+    } else {
+      for (let i = n; i>=1;  i--) {
+        product = product * i
+      }
     }
+
+    return product
   }
 
-  return product
-}
+Solution (recursively): 
+
+  let factorial = (n) => {
+    return n <= 1 ? 1 : factorial(n-1) * n 
+  }
+
+// 11. I love Fibonacci numbers in general, but I must admit I love some more than others.
+
+// I would like for you to write me a function that when given a number (n) returns the n-th number in the Fibonacci Sequence.
+
+// For example:
+
+//    nthFibo(4) == 2
+// Because 2 is the 4th number in the Fibonacci Sequence.
+
+// For reference, the first two numbers in the Fibonacci sequence are 0 and 1, and each subsequent number is the sum of the previous two
+
+  Solution (iterative): 
+
+    let nthFibo = (n) => {
+      let a = 0, b = 1, c = 0
+      if (n<=2) {
+        c = (a+b) * (n-1)
+      } 
+
+      if (n>2) {
+        for (let i = 0; i<n-2; i++) {
+          c = a + b
+          a = b 
+          b = c
+        }
+      }
+
+      return c
+    }
+
+
+  Solution (recursive): 
+
+    let nthFibo = (n) => {
+      let product = 0 
+      if (n===1) {
+        product = 0
+      }
+
+      if (n===2) {
+        product = 1
+      }
+
+      if (n>2) {
+        product = nthFibo(n-1) + nthFibo(n-2)
+      }
+
+      return product
+    }
