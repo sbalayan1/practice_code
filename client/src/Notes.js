b1a26ec925c1d8b705d59ca26df1c4243dc5a633
sbalayan1 <balayans2014@yahoo.com>
2021-10-26 01:27:20 -0700

    codewars kata practice -> fill hard disk drive

--- client/src/Notes.js
+++ client/src/Notes.js
@@ -1228,3 +1228,62 @@ Solution:
     
     return newArr
   }
+
+Best Solution: 
+
+  let number = (a) => a.map((v, i) => `${i + 1}: ${v}`)
+
+
+17. Your task is to determine how many files of the copy queue you will be able to save into your Hard Disk Drive. The files must be saved in the order they appear in the queue.
+
+Input:
+Array of file sizes (0 <= s <= 100)
+Capacity of the HD (0 <= c <= 500)
+Output:
+Number of files that can be fully saved in the HD.
+Examples:
+save([4,4,4,3,3], 12) -> 3
+# 4+4+4 <= 12, but 4+4+4+3 > 12
+save([4,4,4,3,3], 11) -> 2
+# 4+4 <= 11, but 4+4+4 > 11
+Do not expect any negative or invalid inputs.
+
+Solution: 
+  function save(sizes, hd) {
+    let currCapacity = 0
+    let count = 0
+      
+    for (let i = 0; i<sizes.length;i++) {
+      if (sizes.length === 1 && sizes[i] <= hd) {
+        return 1
+      } else if (sizes.length === 1 && sizes[i] > hd ){
+        return 0
+      }
+      
+      if (currCapacity < hd) {
+        currCapacity += sizes[i]
+        count ++
+      }
+      
+      if (currCapacity === hd) {
+        return count
+      }
+      
+      if (currCapacity > hd) {
+        count --
+        return count
+      }
+    }
+    
+    return count
+  }
+
+Best Solution: 
+  function save(sizes, hd) {
+    let i = -1;
+    while (hd >=0) {
+      hd -= sizes.shift();
+      i++;
+    }
+    return i;
+  }
