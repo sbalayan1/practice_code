4bd4f1062afd419e588f387ded4c45ded06a7444
sbalayan1 <balayans2014@yahoo.com>
2021-10-26 00:23:27 -0700

    code wars kata practice

--- client/src/Notes.js
+++ client/src/Notes.js
@@ -1149,3 +1149,26 @@ Solution:
       }
     }
   }
+
+14. Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+Solution: 
+  function lovefunc(flower1, flower2){
+    let isOdd1 = null
+    let isOdd2 = null
+    
+    flower1%2 == 0 ? isOdd1 = false : null
+    flower2%2 == 0 ? isOdd2 = false : null
+    
+    if (isOdd1 === isOdd2) return false
+    
+    return true
+  }
+
+
+Best Solution: 
+  function lovefunc(flower1, flower2){
+    return flower1 % 2 !== flower2 % 2;
+  }
