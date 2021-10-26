b4a3e391e917407368d143909031575204b2b1bf
sbalayan1 <balayans2014@yahoo.com>
2021-10-26 01:10:08 -0700

    codewars kata practice -> rearrange string

--- client/src/Notes.js
+++ client/src/Notes.js
@@ -1203,3 +1203,28 @@ Solution:
     
     return sentence.join('')
   }
+
+16. Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+Examples:
+
+number([]) // => []
+number(["a", "b", "c"]) // => ["1: a", "2: b", "3: c"]
+
+Solution: 
+
+  let number = (arr) => {
+    let count = 0
+    let newArr = []
+    
+    for (let i = 0; i<arr.length; i++) {
+      count ++ 
+      newArr.push(`${count}: ${arr[i]}`)
+    }
+    
+    return newArr
+  }
