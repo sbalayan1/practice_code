86924fa0550b9e4f70b72b1063d5b4ca822548bc
sbalayan1 <balayans2014@yahoo.com>
2022-01-19 18:41:42 -0800

    find duplicate number using cyclic sort

--- client/src/DataStructuresInJS.js
+++ client/src/DataStructuresInJS.js
@@ -1749,3 +1749,21 @@ let findAllMissingNumbers = (nums) => {
 
     return missingNumbers
 }
+
+//Find Duplicate Number using cyclic sort
+function find_duplicate(nums) {
+    let i = 0;
+    while (i < nums.length) {
+      if (nums[i] !== i + 1) {
+        j = nums[i] - 1;
+        if (nums[i] !== nums[j]) {
+          [nums[i], nums[j]] = [nums[j], nums[i]]; // swap
+        } else { // we have found the duplicate
+          return nums[i];
+        }
+      } else {
+        i += 1;
+      }
+    }
+    return -1;
+  }
