9ec575fab4d51a4af1d0df6572c14f1e843d520b
sbalayan1 <balayans2014@yahoo.com>
2022-07-23 08:36:50 -0700

    update

--- DSandAlgos/SlidingWindow.js
+++ DSandAlgos/SlidingWindow.js
@@ -16,67 +16,21 @@
     
 // Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
-
-//Utilize a sliding window to keep track of the substring where windowStart denotes the the start of the substring and windowEnd denotes the end of the substring. I will utilize a hash to keep track of the string's characters and count how many times a certain character has appeared. Loop through the string and do the following at each iteration: 
-    //If the character is a duplicate, increase the character's count and reset the substring's length to 1. If it's not a duplicate, add the new character to the hash with a value of 1, and increase the substring length by 1. Check whether max or the substring is larger and update max. 
-//After we've iterated through the string, return the max variable
-
-//Why this logic is problematic: 
-    //Utilize a hash to keep track of the string's characters and count how many times a certain character has appeared.
-    //If the character is a duplicate, increase the character's count and reset the substring's length to 1
-
-let lengthOfLongestSubString1 = (s) => {
-    let hash = {}, substr = 0, max = 0
+let lengthOfLongestSubstring = (s) => {
+    let hash = {}, windowStart = 0, max = 0
     for (let i = 0; i<s.length; i++) {
-        let currentStr = s[i]
-        if (currentStr in hash) {
-            hash[currentStr] += 1
-            substr = 1
-        } else {
-            hash[currentStr] = 1
-            substr += 1
+        let windowEnd = s[i]
+        while (windowEnd in hash) {
+            hash[windowEnd] === 0 ? delete hash[windowEnd] : hash[windowEnd] --
+            windowStart ++
         }
-
-        max = Math.max(max, substr)
+        
+        hash[windowEnd] = 1
+        max = Math.max(max, i - windowStart + 1)
     }
+    
     return max
 }
-
-    //179 / 987 test cases passed
-    //Input: "dvdf"
-    //Output: 2
-    //Expected 3
-
-var lengthOfLongestSubstring = function(s) {
-    //store characters in hash. If the character is a duplicate, reset the hash?
-    
-    //when a duplicate string is found, you need to update the window and eliminate the duplicate string.
-    let hash = {}, max = 0, currentWindow, windowStart = 0
-    
-    for (let i = 0; i<s.length; i++) {
-        //set the current element to the windowEnd
-        let windowEnd = s[i]
-        
-        //if the windowEnd is in the hash, move the windowStart. 
-        if (windowEnd in hash) {
-            windowStart ++
-        } else {
-            //add the windowEnd to the hash
-            hash[windowEnd] = 1
-        }
-        
-        //update the current window
-        currentWindow = s.slice(windowStart, i+1)
-        console.log(currentWindow)
-        max = Math.max(currentWindow.length, max)
-    }
- 
-    return max
-};
-//91/987 test cases passed
-//input: "pwwkew"
-//output: 4
-//expected: 3
 
 
 let lengthOfLongestSubStr = (str) => {
