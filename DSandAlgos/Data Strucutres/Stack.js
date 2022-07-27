f250acf20d8559dcad5fa2272bd82bd449c63847
sbalayan1 <balayans2014@yahoo.com>
2022-07-27 08:46:58 -0700

    update

--- DSandAlgos/Data Strucutres/Stack.js
+++ DSandAlgos/Data Strucutres/Stack.js
@@ -32,3 +32,18 @@ let reverseString = (s) => {
     return reversed
 }
 
+
+// Write a function that reverses a string. The input string is given as an array of characters s. You must do this by modifying the input array in-place with O(1) extra memory.
+
+//this solution takes advantage of the fact that the string will be reversed at the midpoint. So we don't need to iterate over the entire string. just up to the mid point. At each iteration, we save the target element into a variable, switch the current element into the target's place and the saved target into the current element's place.
+let reverseStringInPlace = (s) => {
+    let end = s.length
+    for (let i = 0; i<Math.floor(s.length/2); i++) {
+        let target = s[end - (i+1)]
+        s[end - (i+1)] = s[i]
+        s[i] = target
+    }
+}
+
+
+
