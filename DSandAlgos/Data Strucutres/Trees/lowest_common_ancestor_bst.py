b86884680de026e689bd9a9789f828b9dce3b7cc
sbalayan1 <balayans2014@yahoo.com>
2022-08-14 06:47:13 -0700

    update

---
+++ DSandAlgos/Data Strucutres/Trees/lowest_common_ancestor_bst.py
@@ -0,0 +1,18 @@
+# Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+
+# According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+
+    #Example 1
+    # Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+    # Output: 6
+    # Explanation: The LCA of nodes 2 and 8 is 6.
+    
+
+def lowest_common_ancestor(root, p, q):
+    if root.val > q.val and root.val > p.val:
+        return self.recursive(root.left, p, q)
+    elif root.val < q.val and root.val < p.val:
+        return self.recursive(root.right, p, q) 
+    else:
+        return root
