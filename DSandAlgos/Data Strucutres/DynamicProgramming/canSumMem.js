1c5f9b916656f833ed45128b1540646f3924ec90
sbalayan1 <balayans2014@yahoo.com>
2022-09-11 06:31:58 -0700

    update

--- DSandAlgos/Data Strucutres/DynamicProgramming/canSumMem.js
+++ DSandAlgos/Data Strucutres/DynamicProgramming/canSumMem.js
@@ -9,11 +9,16 @@ let sumPossible = (amt, arr) => {
     if (amt == 0) return true
 
     for (let n of arr) {
-        if (sumPossible(amt-n, arr) == 1) return true 
+        if (sumPossible (amt-n, arr) == 1) return true 
     }
 
     return false
 }
+
+// m = targetSum
+// n = array length
+// time: O(m^n)
+// space: O(m)
 
 console.log(sumPossible(8, [5, 12, 4])); // -> true, 4 + 4
 console.log(sumPossible(15, [6, 2, 10, 19])); // -> false
@@ -30,6 +35,26 @@ let sumPossibleRecursive = (amt, arr) => {
 
     return false
 }
+
+
+const sumPossible_improved = (amt, nums, memo={}) => {
+    if (amt in memo) return memo[amt]
+    if (amt<0) return false
+    if (amt==0) return true
+  
+    for (let num of nums) {
+      memo[amt] = sumPossible(amt-num, nums, memo)
+      if (sumPossible(amt-num, nums, memo) == true) return true
+    }
+    
+    return false
+  };
+
+// m = targetSum
+// n = array length
+// time:O(m*n)
+// space:(m)
+
 
 // console.log(sumPossibleRecursive(8, [5, 12]))
 // n = 5
