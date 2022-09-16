94027d534b1a6b92b4947a58c59c5fe6ff78ede7
sbalayan1 <balayans2014@yahoo.com>
2022-09-16 08:08:10 -0700

    update

---
+++ DSandAlgos/Data Strucutres/Trees/binaryTreePaths.js
@@ -0,0 +1,11 @@
+// Given the root of a binary tree, return all root-to-leaf paths in any order.
+
+// A leaf is a node with no children
+
+
+let binaryTreePaths = (root) => {
+    let paths = []
+    let buildPaths = (root, path='') => {}
+    buildPaths(root)
+    return paths
+}
