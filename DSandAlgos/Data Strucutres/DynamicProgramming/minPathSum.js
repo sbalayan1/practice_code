4c89ddeb2bd13bd4b0847729eac5b97b0654432f
sbalayan1 <balayans2014@yahoo.com>
2022-09-15 21:59:09 -0700

    update

--- DSandAlgos/Data Strucutres/DynamicProgramming/minPathSum.js
+++ DSandAlgos/Data Strucutres/DynamicProgramming/minPathSum.js
@@ -23,13 +23,13 @@
 
 let minPathSum = (grid, memo={}) => {
     if (grid.length == 0) return null //if there's nothing in the grid
-    if (grid[0][0] in memo) return memo[grid[0][0]]
-    let m = grid.length, n = grid[0].length
-    
+    const m = grid.length, n = grid[0].length, pos = m + ',' + n
+
+    if (pos in memo) return memo[pos]
     if (!m || !n) return null
     if (m == 1 && n == 1) return grid[0][0] //in a 1x1 grid, we return its single value
 
-    let down = [...grid.slice(1)] //recreates the grid, shifted down
+    let down = grid.slice(1) //recreates the grid, shifted down
     let right = grid.map(row => row.slice(1)) //recreate the grid, shifted right
 
     if (minPathSum(right, memo) == null || minPathSum(down, memo) == null) {
@@ -38,10 +38,10 @@ let minPathSum = (grid, memo={}) => {
     }
 
     let smallerPath = Math.min(minPathSum(right, memo), minPathSum(down, memo))
-    memo[grid[0][0]] = smallerPath + grid[0][0]
+    memo[pos] = smallerPath + grid[0][0]
     return smallerPath + grid[0][0]
 }
 
 let grid = [[2,3,4,2,2,5,5,6,6,3],[7,5,6,4,1,7,8,1,7,7],[4,0,4,5,4,2,7,8,9,3],[7,3,8,3,5,0,9,1,8,7],[4,5,4,0,9,5,8,0,8,5],[7,4,7,3,0,1,7,9,0,8],[5,9,1,5,3,7,6,4,8,6]]
 
-console.log(minPathSum(grid)) //=> 7
+console.log(minPathSum(grid)) //=> 52
