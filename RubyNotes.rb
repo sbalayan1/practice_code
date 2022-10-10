c5f7b45cf57b8427acbd46113037a9617981370a
sbalayan1 <balayans2014@yahoo.com>
2022-10-09 18:35:24 -0700

    update

--- RubyNotes.rb
+++ RubyNotes.rb
@@ -4,3 +4,7 @@ class
 instance
     -> is a single occurrence of an obj
     -> instances refer to the individual objects produced from the class
+
+Ruby Object Notation => #<Dog:0x007fc52c2cc588>
+    -> is the default way that ruby communicates to you that you are dealing with an object or instance of a particular class
+    -> the above tells you that the object is an instance of Dog and 0x007fc52c2cc588is its object identifier. This identifier is where the object lives inside the computer.
