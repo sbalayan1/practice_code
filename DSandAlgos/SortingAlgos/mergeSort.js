c64ee5620d706d9e4b2b4d8a1dc17ba5d086b678
sbalayan1 <balayans2014@yahoo.com>
2022-12-27 06:50:05 -1000

    notes on mergeSort and time complexity

--- DSandAlgos/SortingAlgos/mergeSort.js
+++ DSandAlgos/SortingAlgos/mergeSort.js
@@ -1,49 +1,58 @@
-const mergeSort = (arr) => {
-    if (arr.length <= 1) return arr
-    let mid = Math.floor(arr.length/2)
-    return mergeArrays(mergeSort(arr.slice(0, mid)), mergeSort(arr.slice(mid, arr.length)))
-}
-
 const mergeArrays = (left, right) => {
-    let output = []
+    const output = []
     while (left.length > 0 && right.length > 0) {
-        let elem = left[0] < right[0] ? left.shift() : right.shift()
+        const elem = left[0] > right[0] ? right.shift() : left.shift()
         output.push(elem)
     }
 
-    return output.concat(left, right)
+    return output.concat(...left, ...right)
+}
+
+const mergeSort = (arr) => {
+    if (arr.length <= 1) return arr
+    const mid = Math.floor(arr.length/2)
+    return mergeArrays(mergeSort(arr.slice(0, mid)), mergeSort(arr.slice(mid, arr.length)))
 }
 
 const arr = [4,3,5,89,10, 1000, 0, 1]
 console.log(mergeSort(arr))
 console.log(mergeSort(['beg','life','i','to','james brown']))
 
+const worstCase = []
+    //Time Complexity: O(nlogn)
+    //Space Complexity: O(n)
+
+const averageCase = []
+    //Time Complexity: O(nlogn)
+    //Space Complexity: O(n)
+
+const bestCase = [1,2,3,4,5] //array comes presorted
+    //Time Complexity: O(nlogn)
+    //Space Complexity: O(n)
+
+    //is a an out of place, stable algorithm because it requires an extra memory allocation for the output (it does not sort in place) and maintains the order if there are duplicate values in the array
 
 
-// function sort(array) {
-//     if (array.length <= 1){
-//       return array
-//     }
-  
-//     let mid = parseInt(array.length/2, 10)
-//     return merge(sort(array.slice(0,mid)), sort(array.slice(mid)))
-// }
-  
-// function merge(array1, array2) {
-//     let merged = [], arrayIndex1=0, arrayIndex2=0
 
-//     while (arrayIndex1 < array1.length && arrayIndex2 < array2.length) {
-//         if (array1[arrayIndex1].length > array2[arrayIndex2].length) {
-//         merged.push(array2[arrayIndex2])
-//         arrayIndex2 ++
-//         } else {
-//         merged.push(array1[arrayIndex1])
-//         arrayIndex1 ++
-//         }
-//     }
+//Thinking through time complexity, in all cases we will need to split an array into subarrays
+                [1,2,3,4,5]
+               /           \
+            [1, 2]         [3,4,5]
+            /    \          /     \
+          [1]    [2]       [3]    [4,5]
+                                  /    \
+                                [4]    [5]
 
-//     return merged.concat(array1.slice(arrayIndex1), array2.slice(arrayIndex2))
-// }
-  
-  
-//   sort(['beg','life','i','to','james brown'])
+    //The height of the tree is n-1
+
+    //what is the time complexity of the merge operation? 
+        const mergeExample = (left, right) => {
+            let output = []
+            while (left.length > 0 && right.length > 0) {
+                output.push(left[0] > right[0] ? right.shift() : left.shift())
+            }
+
+            return output.concat()
+        }
+
+        //well the while loop only runs n times. Left and right have equal lengths so in all cases, the loop will execute n times. Therefore the time complexity of the merge operation is O(n)
